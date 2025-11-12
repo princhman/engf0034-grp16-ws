3ea937da2e02ce375f2ae0dc2b2dc80d453ec333
Tisha <18traj@gmail.com>
2025-11-12 10:09:29 +0000

    initial commit

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-main.py
-from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session, url_for
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def initial():
+    return redirect(url_for("welcome"))
+
+
+@app.route("/welcome")
+def welcome():
+    return render_template("welcome.html")
+
 
 if __name__ == "__main__":
-    app.run() hello im under the water please help me
+    app.run()
