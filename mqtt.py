de661c755e517875c8726debbf0fb6286e0ddd10
Vladyslav Nalyvaiko <nov.vladkyiv@gmail.com>
2025-11-24 15:14:05 +0000

    update for mqtt

--- mqtt.py
+++ mqtt.py
@@ -1,4 +1,5 @@
-from operator import sub
+import threading
+import time
 
 import paho.mqtt.client as mqtt
 
@@ -7,8 +8,25 @@ def on_connect(client, userdata, flags, rc, properties=None):
     print("Connected with result code " + str(rc))
 
 
+measures = []
+
+
+def print_average():
+    global measures
+    while True:
+        time.sleep(1)
+        if measures:
+            print(f"Average measure: {sum(measures) / len(measures)}")
+            measures = []
+
+
 def on_message(client, userdata, msg):
-    print(f"Message received: {msg.payload.decode()} on topic {msg.topic}")
+    global measures
+    try:
+        measure = float(msg.payload.decode())
+        measures.append(measure)
+    except:
+        pass
 
 
 client = mqtt.Client(callback_api_version=mqtt.CallbackAPIVersion.VERSION2)
@@ -17,10 +35,24 @@ client.on_message = on_message
 client.connect("broker.hivemq.com", 1883, 60)
 
 client.loop_start()
+
+timer_thread = threading.Thread(target=print_average, daemon=True)
+timer_thread.start()
+
 sub_topic = "2025-engf0002/data/stirring"
 pub_topic = "2025-engf0002/action/stirring"
 
-client.publish(pub_topic, "{hello there}")
+
+wanted_rpm = "900"
+client.publish(pub_topic, wanted_rpm)
+client.publish(pub_topic, wanted_rpm)
+
+client.publish(pub_topic, wanted_rpm)
+
+client.publish(pub_topic, wanted_rpm)
+
+client.publish(pub_topic, wanted_rpm)
+
 print("Published message")
 client.subscribe(sub_topic)
 input = input("Press Enter to exit")
