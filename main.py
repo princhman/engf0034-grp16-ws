112ba888a2eefb3d70e4c942841e55af7c6868eb
Vladyslav Nalyvaiko <nov.vladkyiv@gmail.com>
2025-12-07 17:57:03 +0000

    fix

--- main.py
+++ main.py
@@ -277,4 +277,7 @@ if __name__ == "__main__":
             },
         )
     client.loop_start()
-    app.run()
+    import os
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
